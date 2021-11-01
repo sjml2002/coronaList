36ebfa477ffe1aaafb08a20c0db8c95c6d965586
sjml2002 <sjml2002@naver.com>
2021-11-01 15:50:45 +0900

    메인interface 구현

--- include/hashTableArr.h
+++ include/hashTableArr.h
@@ -12,16 +12,28 @@ typedef struct InfectedPerson {
 	char region[50]; //지역
 } IP;
 
-//함수 원형 
+
+
+///// 함수 원형 /////
 int IParrSize(IP* ip);
 void IPpush(IP* ip, int ID, int gender, int age, char* region);
 void IPsearch(IP* ip, int ID);
+void IPinit(IP* ip, int start, int end);
 IP* tableSizeUP(IP* arr, int size);
 IP* tableSizeDown(IP* arr, int size);
 
-//함수 작성 
+
+
+///// 함수 작성 /////
 int IParrSize(IP* ip) {
 	return _msize(ip) / sizeof(IP);
+}
+
+void IPinit(IP* ip, int start, int end) {
+	while (start < end) {
+		ip[start].ID = -1;
+		start++;
+	}
 }
 
 void IPpush(IP* ip, int ID, int gender, int age, char* region) {
@@ -32,7 +44,7 @@ void IPpush(IP* ip, int ID, int gender, int age, char* region) {
 }
 
 void IPsearch(IP* ip, int ID) {
-	if (ID == -1) {
+	if (ID == 0) {
 		printf("\n---- 확진자 전체 검색 ----\n");
 		int size = IParrSize(ip);
 		int i = 1;
@@ -43,15 +55,21 @@ void IPsearch(IP* ip, int ID) {
 			printf("region: %s\n", ip[i].region);
 			if (ip[i].ID + 1 != ip[i+1].ID)
 				break;
+			printf("\n"); 
 			i++;
 		}
 	}
 	else {
-		printf("\n---- 확진자 ID: %d 검색 ----\n", ip[ID].ID);
-		printf("gender: %d\n", ip[ID].gender);
-		printf("age: %d\n", ip[ID].age);
-		printf("region: %s\n", ip[ID].region);
-		printf("\n");
+		//선언이 아예 안된 경우거나 선언이 됐지만 데이터가 들어가지 않은경우 
+		if(ip[ID].ID == -1 || ID < 0 || ID >= IParrSize(ip)){
+			printf("\n확진자가 존재하지 않습니다.\n");
+		}
+		else {
+			printf("\n---- 확진자 ID: %d 검색 ----\n", ip[ID].ID);
+			printf("gender: %d\n", ip[ID].gender);
+			printf("age: %d\n", ip[ID].age);
+			printf("region: %s\n", ip[ID].region);
+		}
 	}
 	printf("---- 확진자 검색 종료 ----\n\n");
 }
@@ -61,6 +79,7 @@ IP* tableSizeUP(IP* arr, int size) {
 	free(arr);
 	arr = '\0';
 	arr = (IP*)malloc(size + (10 * sizeof(IP)));
+	IPinit(arr, size, size + 10);
 	return arr;
 }
 
--- index.c
+++ index.c
@@ -2,7 +2,7 @@
 #include "./include/hashTableArr.h"
 #include "./include/stringLib.h"
 
-void tmpScan(int* gender, int *age, char region[50]) {
+void dataScan(int* gender, int *age, char region[50]) {
 	printf("확진자의 정보를 입력해주세요.\n");
 	printf("gender: ");
 	scanf("%d", gender);
@@ -17,25 +17,47 @@ void tmpScan(int* gender, int *age, char region[50]) {
 
 int main(int argc, char** argv) {
 	IP* IPtable = (IP*)malloc(2 * sizeof(IP));
-	int gender;
-	int age;
-	char region[50];
-	
-	int index = 1;
-	while (index < 5) {
-		printf("%d: ", index);
-		tmpScan(&gender, &age, region);
-		//확진자를 추가할려는데
-		//배열에 데이터가 꽉찼다면
-		//배열의 크기를 늘림
-		if (index >= IParrSize(IPtable)) {
-			tableSizeUP(IPtable, _msize(IPtable));
+	IPinit(IPtable, 0, 2);
+	char order;
+	int IPpushIndex = 1;
+
+	printf("[주문] 0: 프로그램 종료, 1: 확진자 추가, 2: 확진자 검색\n");
+	while (1) {
+		printf("[주문]: ");
+		scanf("%c", &order);
+		getchar(); //입력버퍼 비우기
+		if (order == '0') { //프로그램 종료
+			printf("프로그램을 종료합니다.\n");
+			return 0;
 		}
-		IPpush(&IPtable[index], index, gender, age, region);
-		IPsearch(IPtable, index);
-		index++;
+		else if (order == '1') {  // 데이터 추가
+			int gender;
+			int age;
+			char region[50];
+			
+			dataScan(&gender, &age, region);
+			//확진자를 추가할려는데
+			//배열에 데이터가 꽉찼다면
+			//배열의 크기를 늘림
+			if (IPpushIndex >= IParrSize(IPtable)) {
+				printf("%d\n", IParrSize(IPtable));
+				tableSizeUP(IPtable, _msize(IPtable));
+			}
+			IPpush(&IPtable[IPpushIndex], IPpushIndex, gender, age, region);
+			IPpushIndex++;			
+		}
+		else if (order == '2') { // 데이터 검색
+			int searchIndex;
+			printf("검색할 ID를 입력해주세요. (0 입력시 전체 검색이 됩니다.)\n");
+			printf("ID: ");
+			scanf("%d", &searchIndex);
+			getchar(); //입력버퍼 지우기
+			IPsearch(IPtable, searchIndex);
+		}
+		else {
+			printf("다시 입력해주세요.\n");
+		}
 	}
-	IPsearch(IPtable, -1); //전체 검색 
 	
 	return 0;
 }
