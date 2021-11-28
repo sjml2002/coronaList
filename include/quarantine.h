189067d6cbbd76f1680f71f182a6a45b4d3a76f0
sjml2002 <sjml2002@naver.com>
2021-11-28 17:24:35 +0900

    밀접접촉자 삭제기능 추가

--- include/quarantine.h
+++ include/quarantine.h
@@ -18,7 +18,8 @@ typedef struct Quarantine {
 int QTlinkedListSize(QT* qt);
 void QTpush(QT* qt, char* vaccineName, int vaccination);
 void QTsearchView(QT* qt, int IPID);
-int QTsize(QT* qt);
+int QTremove(QT* qt, int QID);
+void QTremoveAll(QT* qt);
 
 
 ///// 함수 작성 /////
@@ -34,33 +35,70 @@ int QTlinkedListSize(QT* qt) {
 //밀접접촉자 추가 
 void QTpush(QT* qt, char* vaccineName, int vaccination) {
 	QT* tmp = qt; 
-	while (tmp->nextQT != '\0') { //링크드 리스트의 맨 끝까지 이동
-		tmp = tmp->nextQT;
+	if (vaccineName[0] == 'h') { //head init
+		qt->ID = -1;
 	}
-	//값 할당 
-	tmp->ID = QTlinkedListSize(qt);
-	tmp->vaccination = vaccination;
-	ct_strcpy(tmp->vaccineName, vaccineName);
+	else { //정상 push
+		int prevID = 0;
+		while (tmp->nextQT != '\0') { //링크드 리스트의 맨 끝까지 이동
+			prevID = tmp->ID;
+			tmp = tmp->nextQT;
+		}
+		//값 할당 
+		tmp->ID = prevID + 1;
+		tmp->vaccination = vaccination;
+		ct_strcpy(tmp->vaccineName, vaccineName);
+	}
 	//새로운 노드 메모리 할당 및 nextQT를 nullptr로 초기화
 	QT* newNode = (QT*)malloc(sizeof(QT)); 
 	newNode->nextQT = '\0';
 	//현재 nextQT 값 할당
-	tmp->nextQT = newNode; 
+	tmp->nextQT = newNode;
 }
 
-//밀접접촉자 검색 
+//밀접접촉자 전체 검색 
 void QTsearchView(QT* qt, int IPID) {
 	const char* vacStr[4] = {
 		"미접종", "1차접종", "2차접종 후 14일 미경과", "접종완료"
 	};
 	printf("\n---- 확진자ID: %d의 밀접접촉자 전체 검색 ----\n", IPID);
 	while (qt->nextQT != 0) {
-		printf("ID: %d\n", qt->ID);
-		printf("백신이름: %s , %s\n", qt->vaccineName, vacStr[qt->vaccination]);
-		printf("\n");
+		if(qt->ID != -1) {
+			printf("QID: %d\n", qt->ID);
+			printf("백신이름: %s , %s\n", qt->vaccineName, vacStr[qt->vaccination]);
+			printf("\n");
+		}
 		qt = qt->nextQT;
 	}
 	printf("---- 밀접접촉자 검색 종료 ----\n\n");
+}
+
+//밀접접촉자 1명 삭제
+int QTremove(QT* qt, int QID) {
+	QT* prevQT = qt; //이전 QT포인터 임시 저장
+	qt = qt->nextQT; 
+	while (qt->nextQT != 0) { 
+		if (qt->ID == QID) {
+			prevQT->nextQT = qt->nextQT;
+			qt->nextQT = 0;
+			free(qt);
+			qt = 0;
+			return 1;
+		}
+		prevQT = qt;
+		qt = qt->nextQT;
+	}
+	return 0;
+}
+
+//밀접접촉자 전체 삭제 
+void QTremoveAll(QT* qt) {
+	if(qt->nextQT != 0) {
+		QTremoveAll(qt->nextQT);
+		qt->nextQT = 0;
+	}
+	free(qt);
+	qt = 0;
 }
 
 #endif
